8730b73049db3a257f200d502e76e0bde6ee4a9b
Matúš <kollarm9@gmail.com>
2025-03-31 10:37:50 +0200

    add basic metadata localization

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,11 +1,38 @@
+import type { Metadata, ResolvingMetadata } from 'next'
+
 import { getDictionary } from './dictionaries'
+
+type Props = {
+  params: Promise<{ lang: 'en' | 'sk' }>
+}
+
+export async function generateMetadata({
+  params,
+  }: Props, parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { lang } = await params
+  const dict = await getDictionary(lang)
+  return {
+    title: dict.head.title,
+    description: dict.head.description,
+    icons: {
+      icon: '/favicon.ico',
+    }
+  }
+}
 
 export default async function Page({
   params,
-}: {
-  params: Promise<{ lang: 'en' | 'sk' }>
-}) {
+}: Props) {
   const { lang } = await params
-  const dict = await getDictionary(lang) // en
-  return <h1>{dict.welcome}</h1> // Add to Cart
+  const dict = await getDictionary(lang)
+  return (
+    <>
+      <header>
+      </header>
+      <main>
+
+      </main>
+    </>
+  )
 }
